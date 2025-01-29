271a324038473b95959e23b1726432a6ffd8d3ce
ankithsavio <ankithsavio@gmail.com>
2025-01-29 17:15:34 +0000

    minor updates - init integration of search

--- researcher/search.py
+++ researcher/search.py
@@ -1,17 +1,26 @@
-import requests
-import httpx
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+import requests
+import asyncio
+from crawl4ai import AsyncWebCrawler
+from qdrant_client import QdrantClient
 
 
 class SearXNG:
     def __init__(self):
-        self.httpx_client = httpx.Client(verify=False)
         self.instance = "http://localhost:8888/search"
         self.params = {
             "format": "json",
             "categories": "general",
             "language": "en",
         }
+        self.text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=300,
+            chunk_overlap=50,
+            length_function=len,
+        )
+
+        self.vectordb = QdrantClient(url="http://localhost:6333")
+        self.collection_name = "WebSearch"
 
     def get_urls(self, query):
         self.params |= {"q": query}
@@ -21,19 +30,24 @@ class SearXNG:
 
             data = response.json()
 
-            print(f"Total results: {len(data['results'])}")
-            for result in data["results"]:
-                print(f"\nTitle: {result['title']}")
-                print(f"URL: {result['url']}")
-                print(f"Content: {result['content'][:100]}...")
+            return [result["url"] for result in data["results"]]
 
         except requests.exceptions.RequestException as e:
             print(f"Request failed: {e}")
         except KeyError as e:
             print(f"Unexpected JSON format: {e}")
 
-    def get_htmls(self):
-        pass
+    def get_web_content(self, url: str) -> str:
 
-    def get_articles_chunks(self):
+        async def _crawl():
+            async with AsyncWebCrawler() as crawler:
+                result = await crawler.arun(
+                    url=url,
+                )
+                return result
+
+        return asyncio.run(_crawl())
+
+    def process_documents(self, docs):
+        # TODO
         pass
