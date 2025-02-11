e226d5a1d2164833cd339aec9a9bb2914bc5773b
ankithsavio <ankithsavio@gmail.com>
2025-02-11 17:51:14 +0000

    create company portfolio

--- resume_writer/engine.py
+++ resume_writer/engine.py
@@ -1,17 +1,18 @@
 from dotenv import load_dotenv
 
 load_dotenv(".env")
-
+from typing import List, Dict
 from pydantic import BaseModel
 from resume_writer.utils.formats.prompt import Prompt
 from resume_writer.utils.formats.user import UserReport
 from resume_writer.utils.formats.company import CompanyReport
 from resume_writer.utils.formats.search import SearchQueries
 from llms.extractor import PDFExtractor
-from llms.basellm import TogetherBaseLLM, GeminiBaseStructuredLLM
+from llms.basellm import TogetherBaseLLM, GeminiBaseStructuredLLM, HfBaseLLM
 from resume_writer.utils.prompts import PDF_PROMPT, JD_PROMPT, JOB_DESC, QUERY_PROMPT
 from researcher.search import SearXNG
 import json
+import time
 
 
 class ResumeWriterEngine:
@@ -49,6 +50,34 @@ class ResumeWriterEngine:
             format=CompanyReport,
         )
         return [result]
+
+    def summarize_search_results(self, results: List[Dict[str, str]]):
+        summarized_results = []
+        for result in results:
+            combined_results = "\n\n".join(str(item) for item in result["result"])
+            response = self.llm(
+                str(
+                    Prompt(
+                        prompt=f"""You are a helpful researcher. You are provided with the user query and a list of web search results.
+                        <Instructions> 
+                        1. Summarize the search results into clear and concise overview that directly address the search query.
+                        2. Synthesize information from multiple results where appropriate.
+                        3. Include citations for key facts and claims. For each piece of information you present, indicate which search result(s) it came from.
+                        4. Prioritize information relevance, omit irrelevant or tangential details.
+                        5. Maintain a neutral and objective tone.
+                        6. The summary should be no more than 500 words followed by Sources.
+                        </Instructions> 
+                        <User_Query>
+                        {result['query']}
+                        </User_Query>
+                        <Result_List>
+                        {combined_results}
+                        </Result_List>"""
+                    )
+                )
+            )
+            summarized_results.append({"summary": response} | result)
+        return summarized_results
 
     def create_user_portfolio(self):
         portfolio_outline = self.llm(
@@ -93,6 +122,7 @@ class ResumeWriterEngine:
                         # Title
                         ## Subsection Title
                         ### Subsubsection Title
+                        # Sources
                         """
                     ],
                 )
@@ -108,21 +138,39 @@ class ResumeWriterEngine:
             ),
             format=SearchQueries,
         )
-        search_queires = result
+        search_queires = [query for item in result for query in item[1].queries[:2]]
+        search_results = self.summarize_search_results(
+            self.search.run_many(queries=search_queires)
+        )
 
-        def search_web(queries):
-            results = []
-            for query in queries:
-                result = self.search.run(query)
-                results.append({"query": query, "results": result})
-                break  # TODO : fix multi query error
-            return results
+        search_results_formatted = "\n".join(
+            f"""<Query {idx}>\n{item['query']}\n</Query {idx}>\n<Result {idx}>\n{item['summary']}\n</Result {idx}>"""
+            for idx, item in enumerate(search_results)
+        )
 
-        results = search_web(search_queires.products_services.queries)
-        return results
+        self.company_portfolio = self.llm(
+            str(
+                Prompt(
+                    prompt=f"""Generate a single company report article on {self.company_reports[0].company.name} using the provided outline and a list of search queries and their results.
+                        <Outline>
+                        {portfolio_outline}
+                        </Outline>
+                        <Search Results>
+                        {search_results_formatted}
+                        </Search Results>
+                        """
+                )
+            )
+        )
+
+        return self.company_portfolio
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     model = ResumeWriterEngine()
     results = model.create_company_portfolio()
+    end_time = time.time()
+
     print(results)
+    print(f"Execution time: {end_time - start_time} seconds")
