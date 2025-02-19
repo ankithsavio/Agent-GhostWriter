877be4f1096c3fc3264b619be438f40f15ae8b24
ankithsavio <ankithsavio@gmail.com>
2025-02-19 14:14:07 +0000

    init commit

---
+++ ghost_writer/modules/knowledgebase.py
@@ -0,0 +1,1 @@
+# TODO : add knowledge bases creation modules - load files -> optional(anonymize) -> document -> RAG
---
+++ ghost_writer/modules/storm.py
@@ -0,0 +1,1 @@
+# TODO : storm workflow - peronsa creation -> parallel grounded conversation -> conversation history
