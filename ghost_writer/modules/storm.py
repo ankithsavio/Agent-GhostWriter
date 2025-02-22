5863ade8cfb2ab2a03d914d08f462e25d587440f
ankithsavio <ankithsavio@gmail.com>
2025-02-22 19:58:25 +0000

    add general prompts to storm module

--- ghost_writer/modules/storm.py
+++ ghost_writer/modules/storm.py
@@ -1,6 +1,9 @@
+from typing import List
 from pydantic import BaseModel
 from ghost_writer.utils.prompt import Prompt
+from ghost_writer.utils.persona import Personas
 from ghost_writer.utils.workers import Worker, Message
+from ghost_writer.modules.vectordb import Qdrant
 from ghost_writer.modules.knowledgebase import KnowledgeBaseBuilder
 from llms.basellm import TogetherBaseLLM, GeminiBaseStructuredLLM
 
@@ -11,13 +14,13 @@ class Storm:
         self.llm = TogetherBaseLLM()
         self.struct_llm = GeminiBaseStructuredLLM()
 
-    def get_personas(self, model: BaseModel, prompt: Prompt):
+    def get_personas(self, prompt: Prompt):
         """
         Get personas as workers with conversation history
         """
         personas_result = self.struct_llm(
             prompt=str(prompt),
-            format=model,
+            format=Personas,
         )
         return [Worker(**persona.model_dump()) for persona in personas_result.editors]
 
@@ -25,25 +28,44 @@ class Storm:
         """
         Generate questions from the worker
         """
-        response = self.llm(str(prompt))
+        response = self.llm(
+            str(prompt)
+            + str(
+                Prompt(
+                    prompy="\n",
+                    persona=f"""
+                        role: {worker.persona} 
+                        description: {worker.description} 
+                        """,
+                    conversation_history="\n".join(
+                        [
+                            f"\nrole : {message.role}\ncontent: {message.content}"
+                            for message in worker.conversation.get_messages()
+                        ]
+                    ),
+                )
+            )
+        )
         message = Message(role=worker.persona, content=response)
         worker.conversation.add_message(message)
         return message
 
-    def get_search_result(
-        self,
-        model: BaseModel,
-        knowledge_base: KnowledgeBaseBuilder,
-        prompt: Prompt,
-    ):
+    def get_search_queries(self, worker: Worker, model: BaseModel, prompt: Prompt):
         """
         Get search results from the knowledge base.
         """
+        last_message = worker.conversation.get_messages()[-1]
         response = self.struct_llm(
-            str(prompt),
+            str(
+                Prompt(
+                    prompt=f"You want to answer the {worker.persona}'s question by querying vector database. What queries would answer the question effectively?",
+                    question=f"role: {last_message.role}\ncontent: {last_message.content}",
+                )
+            )
+            + str(prompt),
             format=model,
         )
-        return knowledge_base.query_vectordb(**response.model_dump())
+        return response
 
     def get_answers(self, worker: Worker, prompt: Prompt):
         """
