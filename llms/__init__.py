3dc585d448e0c139fcd8afa69c2941484684ab4b
ankithsavio <ankithsavio@gmail.com>
2025-03-05 18:07:35 +0000

    minor fixes

--- llms/__init__.py
+++ llms/__init__.py
@@ -1,4 +1,3 @@
 from .llms import GenLLM, OptimLLM, GeminiLLM
-from .extractor import PDFExtractor
 
-__all__ = ["GenLLM", "OptimLLM", "GeminiLLM", "PDFExtractor"]
+__all__ = ["GenLLM", "OptimLLM", "GeminiLLM"]
